18a0e51689d15f4ca9ab49fdff0498e8b2398e91
0xTranqui <thinkandchill11@gmail.com>
2023-09-23 12:14:43 -0400

    updated to something that actually builds lets go

--- src/core/router/RouterWallet.sol
+++ src/core/router/RouterWallet.sol
@@ -5,9 +5,9 @@ import "./aa/BaseAccount.sol";
 import "./utils/TokenCallbackHandler.sol";
 import {FundsReceiver} from "../../utils/FundsReceiver.sol";
 import {ReentrancyGuard} from "openzeppelin-contracts/security/ReentrancyGuard.sol";
-import "openzeppelin-contract/utils/cryptography/ECDSA.sol";
+import "openzeppelin-contracts/utils/cryptography/ECDSA.sol";
 import "openzeppelin-contracts/proxy/utils/Initializable.sol";
-import "openzeppelin-contractsproxy/utils/UUPSUpgradeable.sol";
+import "openzeppelin-contracts/proxy/utils/UUPSUpgradeable.sol";
 
 /**
  * @title RouterWallet
@@ -45,7 +45,9 @@ contract RouterWallet is
     //////////////////////////////////////////////////
 
     error Single_Target_Call_Failed(SingleTargetInputs inputs);
-    error Sender_Not_EntryPoint;
+    error Call_Via_EntryPoint_Failed();
+    error Sender_Not_EntryPoint();
+    error Insufficient_Balance();
 
     //////////////////////////////////////////////////
     // STORAGE
@@ -87,6 +89,27 @@ contract RouterWallet is
     // FUNCTIONS
     //////////////////////////////////////////////////
 
+
+    function executeViaEntryPoint(bytes calldata userOpCalldata) external payable nonReentrant {
+        if (msg.sender != address(_entryPoint)) revert Sender_Not_EntryPoint();
+        /*
+            userOpCalldata breakdown:
+            bytes 0-20 will always contain the encoded target address
+            bytes 20-52 will always contain the encoded uint256 msg.value needed for downstream fees
+            bytes 52-end will always contain the encoded calldata to call the downstream target with
+            bytes 52-72 will always contain the address of the original signer to pass through
+                validation of signature authenticity happens during `validateUserOp` required
+                by compatible wallets to have as defined in IAccount
+        */
+        // address target = userOpCalldata[0:20];
+        (address target) = abi.decode(userOpCalldata[0:20], (address));
+        (uint256 value) = abi.decode(userOpCalldata[20:52], (uint256));
+        (address originalSigner) = abi.decode(userOpCalldata[52:72], (address));
+        if (entryPointDepositMirror[originalSigner] < value) revert Insufficient_Balance();
+        (bool success,) = target.call{value: value}(userOpCalldata[52:]);
+        if (!success) revert Call_Via_EntryPoint_Failed();        
+    }
+
     /// implement template method of BaseAccount
     /// NOTE: this isnt fully it because there isnt a check for if the UserOp is processed or not
     /// Probably need to do the deposit mirroring accounting somewhere else
@@ -98,60 +121,67 @@ contract RouterWallet is
         override
         returns (uint256 validationData)
     {
+
         // Recover address that signed transaction to be submitted through River wallet
         bytes32 hash = userOpHash.toEthSignedMessageHash();
-        address originalSigner = hash.recover(userOp.signature);
-        uint256 msgValueAndGas = msg.value + userOp.preVerificationGas;
-        // Return fail value (1) if the deposit mirror balance of original signer is less than msg.value
-        if (entryPointDepositMirror[originalSigner] < msgValueAndGas) return SIG_VALIDATION_FAILED;
-        // If valid signature, then deduct msg.value + gas from the mirror deposit tracker
-        entryPointDepositMirror[originalSigner] -= msgValueAndGas;
-        return 0;    
+        address originSigner = hash.recover(userOp.signature);
+        (address passThroughSigner) = abi.decode(userOp.callData[4:24], (address));
+        // Signal op failutre if the signer being passed into calldata isnt the signer of the userOp
+        if (originSigner != passThroughSigner) return SIG_VALIDATION_FAILED;
+        return 0;       
     }
 
-    /**
-     * check current account deposit in the entryPoint
-     */
-    function getDeposit() public view returns (uint256) {
-        return entryPoint().balanceOf(address(this));
-    }
+    /// @inheritdoc BaseAccount
+    function entryPoint() public view virtual override returns (IEntryPoint) {
+        return _entryPoint;
+    }    
 
-    /**
-     * check current account deposit in river wallet for given sender
-     */
-    function getRiverDepositForAccount(address account) public view returns (uint256) {
-        return entryPointDepositMirror[account];
-    }
+    /// Hardcoding no ability to upgrade function for the moment
+    function _authorizeUpgrade(address newImplementation) internal view override {}
 
-    /**
-     * deposit more funds for this account in the entryPoint
-     * AND track what account to deposit those funds for in RiverWallet
-     */
-    function addDeposit(address account) public payable {
-        entryPoint().depositTo{value: msg.value}(address(this));
-        entryPointDepositMirror[account] = msg.value;
-    }
+    // /**
+    //  * check current account deposit in the entryPoint
+    //  */
+    // function getDeposit() public view returns (uint256) {
+    //     return entryPoint().balanceOf(address(this));
+    // }
 
-    /**
-     * withdraw value from the River Wallet's deposit in entry point
-     * ensure that sender is not withdrawing more than they have allocated to River wallet
-     * @param withdrawAddress target to send to
-     * @param amount to withdraw
-     */
-    function withdrawDepositTo(address payable withdrawAddress, uint256 amount) public {
-        if (amount > entryPointDepositMirror[msg.sender]) revert Withdrawal_Exceeds_Funds();
-        entryPoint().withdrawTo(withdrawAddress, amount);
-    }
+    // /**
+    //  * check current account deposit in river wallet for given sender
+    //  */
+    // function getRiverDepositForAccount(address account) public view returns (uint256) {
+    //     return entryPointDepositMirror[account];
+    // }
 
-    //
-    function callTarget(SingleTargetInputs calldata callInputs) external payable nonReentrant {
-        (bool success,) = callInputs.target.call{value: msg.value}(
-            abi.encodePacked(callInputs.selector, abi.encode(msg.sender, callInputs.data))
-        );
-        if (!success) revert Single_Target_Call_Failed(callInputs);
-    }
+    // /**
+    //  * deposit more funds for this account in the entryPoint
+    //  * AND track what account to deposit those funds for in RiverWallet
+    //  */
+    // function addDeposit(address account) public payable {
+    //     entryPoint().depositTo{value: msg.value}(address(this));
+    //     entryPointDepositMirror[account] = msg.value;
+    // }
 
-    function executeViaEntryPoint() external payable {
-        if (msg.sender != address(entryPoint())) revert Sender_Not_EntryPoint();
-    }
+    // /**
+    //  * withdraw value from the River Wallet's deposit in entry point
+    //  * ensure that sender is not withdrawing more than they have allocated to River wallet
+    //  * @param withdrawAddress target to send to
+    //  * @param amount to withdraw
+    //  */
+    // function withdrawDepositTo(address payable withdrawAddress, uint256 amount) public {
+    //     if (amount > entryPointDepositMirror[msg.sender]) revert Withdrawal_Exceeds_Funds();
+    //     entryPoint().withdrawTo(withdrawAddress, amount);
+    // }
+
+    // //
+    // function callTarget(SingleTargetInputs calldata callInputs) external payable nonReentrant {
+    //     (bool success,) = callInputs.target.call{value: msg.value}(
+    //         abi.encodePacked(callInputs.selector, abi.encode(msg.sender, callInputs.data))
+    //     );
+    //     if (!success) revert Single_Target_Call_Failed(callInputs);
+    // }
+
+    // function executeViaEntryPoint() external payable {
+    //     if (msg.sender != address(entryPoint())) revert Sender_Not_EntryPoint();
+    // }
 }
