e8368f9119ffc935ba3002a8f19177298b56a9d8
0xTranqui <thinkandchill11@gmail.com>
2023-11-13 21:22:34 -0500

    update regiser and update functions to take in an input

--- src/interfaces/INodeRegistry.sol
+++ src/interfaces/INodeRegistry.sol
@@ -22,20 +22,22 @@ interface INodeRegistry {
      * 
      * @dev Callable by anyone
      *
+     * @param userId        UserId to associate messages with
      * @param schema        Schema initialize node as
      * @param messages      Messages to send to initialized node
      */
-    function register(bytes32 schema, bytes[] calldata messages) external returns (uint256);
+    function register(uint256 userId, bytes32 schema, bytes[] calldata messages) external returns (uint256);
 
     /**
      * @notice Batch version of `register`
      *
      * @dev Will revert messages.length < schemas.length !!
      *
+     * @param userIds       UserIds to associate messages with
      * @param schemas       Schemas to register nodes as
      * @param messages      Messages to send to registered nodes
      */
-    function registerBatch(bytes32[] calldata schemas, bytes[][] calldata messages) external returns (uint256[] memory);
+    function registerBatch(uint256[] calldata userIds, bytes32[] calldata schemas, bytes[][] calldata messages) external returns (uint256[] memory);
 
     //////////////////////////////////////////////////
     // UPDATE
@@ -46,18 +48,20 @@ interface INodeRegistry {
      *
      * @dev Callable by anyone
      *
+     * @param userId        UserId to associate messages with
      * @param nodeId        Id of node to target
      * @param messages      Messages to send to target node
      */
-    function update(uint256 nodeId, bytes[] calldata messages) external;
+    function update(uint256 userId, uint256 nodeId, bytes[] calldata messages) external;
 
     /**
      * @notice Batch version of `update`
      *
      * @dev Will revert messages.length < schemas.length !!
      *
+     * @param userIds       UserIds to associate messages with     
      * @param nodeIds       Ids of nodes to target
      * @param messages      Messages to send to target nodes
      */
-    function updateBatch(uint256[] calldata nodeIds, bytes[][] calldata messages) external;
+    function updateBatch(uint256[] calldata userIds, uint256[] calldata nodeIds, bytes[][] calldata messages) external;
 }
